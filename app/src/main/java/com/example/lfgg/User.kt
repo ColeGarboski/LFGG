401ef738d581a5d3b92ef827b8af4c4fb3d5aa1a
slt1021 <99755259+slt1021@users.noreply.github.com>
2023-03-27 22:47:30 -0400

    Added Platform and Game on Signup
    Uses spinners to have drop down menu for both platforms and games that the admins chose to allow users select.

--- app/src/main/java/com/example/lfgg/User.kt
+++ app/src/main/java/com/example/lfgg/User.kt
@@ -3,13 +3,17 @@ package com.example.lfgg
 class User {
     var name: String? = null
     var email: String? = null
+    var platform: String? = null
+    var game: String? = null
     var uid: String? = null
 
     constructor(){}
 
-    constructor(name: String, email: String, uid: String) {
+    constructor(name: String, email: String, platform: String, game: String, uid: String) {
         this.name = name
         this.email = email
+        this.platform = platform
+        this.game = game
         this.uid = uid
     }
 }
