706ab44c9d3eac458a09dee1c1ddf26132b302d4
Cole Garboski <cole.garboski@snhu.edu>
2023-04-10 20:41:54 -0400

    Begins adding bubbles next to each chat to display who sent it more easily

--- app/src/main/java/com/example/lfgg/MessageAdapter.kt
+++ app/src/main/java/com/example/lfgg/MessageAdapter.kt
@@ -1,7 +1,7 @@
 package com.example.lfgg
 
 import android.content.Context
-import android.provider.Telephony.Mms.Sent
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -51,7 +51,64 @@ class MessageAdapter(val context: Context, val messageList: ArrayList<Message>):
             }.addOnFailureListener {
                 holder.receivedMessageUser.text = "Unknown"
             }
+            //Set the color of chat's user bubble
+            val bubbleTextView = viewHolder.itemView.findViewById<TextView>(R.id.txtUserPicBubble)
+
+            val UID = currentMessage.senderId!!
+
+            val hexColor = usernameToColor(UID)
+            val invertedHexColor = invertColor(hexColor)
+
+            val invertedBubbleColor = Color.parseColor(invertedHexColor)
+            val bubbleColor = Color.parseColor(hexColor)
+
+            bubbleTextView.setBackgroundColor(bubbleColor)
+            //bubbleTextView.setTextColor(invertedBubbleColor)
+
+            mDbRef.child("user").child(currentMessage.senderId!!).child("name").get().addOnSuccessListener {
+                val username = it.value.toString()
+                bubbleTextView.text = username.substring(0, 1).uppercase()
+                //println(username)
+                //println(username.substring(0, 1).uppercase())
+            }.addOnFailureListener {
+            }
         }
+    }
+
+    fun usernameToColor(username: String): String {
+        // Convert the username to a character array
+        val chars = username.toCharArray()
+
+        // Compute the hash value
+        var hash = 0
+        for (char in chars) {
+            hash += char.toInt()
+        }
+
+        // Generate red, green, and blue values using the hash value
+        val red = hash % 256
+        val green = (hash / 256) % 256
+        val blue = (hash / 256 / 256) % 256
+
+        // Combine the color components into an ARGB hex string
+        val color = StringBuilder("#AABBCCDD")
+        color.replace(3, 5, red.toString(16).padStart(2, '0'))
+        color.replace(5, 7, green.toString(16).padStart(2, '0'))
+        color.replace(7, 9, blue.toString(16).padStart(2, '0'))
+
+        return color.toString()
+    }
+
+    fun invertColor(hexColor: String): String {
+        val red = hexColor.substring(3, 5).toInt(16)
+        val green = hexColor.substring(5, 7).toInt(16)
+        val blue = hexColor.substring(7, 9).toInt(16)
+
+        val invertedRed = 255 - red
+        val invertedGreen = 255 - green
+        val invertedBlue = 255 - blue
+
+        return "#AA${invertedRed.toString(16).padStart(2, '0')}${invertedGreen.toString(16).padStart(2, '0')}${invertedBlue.toString(16).padStart(2, '0')}"
     }
 
     override fun getItemViewType(position: Int): Int {
